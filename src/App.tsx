6e6cb025cdfd1bc03bd927c9614638932e518311
yansobol <yanchoper@gmail.com>
2022-10-19 03:58:20 +0300

    Separated bundle to chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import "./App.css";
 import "./scss/app.scss";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Cart from "./pages/Cart";
-import Pizza from "./pages/Pizza";
+
+const Header = lazy(
+  () => import(/*webpackChunkName: 'Header'*/ "./components/Header")
+);
+const Home = lazy(() => import(/*webpackChunkName: 'Home'*/ "./pages/Home"));
+const NotFound = lazy(
+  () => import(/*webpackChunkName: 'NotFound'*/ "./pages/NotFound")
+);
+const Cart = lazy(() => import(/*webpackChunkName: 'Cart'*/ "./pages/Cart"));
+const Pizza = lazy(() => import(/*webpackChunkName: 'Pizza'*/ "./pages/Pizza"));
 
 function App() {
   return (
     <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/pizza/:id" element={<Pizza />} />
-            <Route path="/404" element={<NotFound />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+        <div className="content">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/pizza/:id" element={<Pizza />} />
+              <Route path="/404" element={<NotFound />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Suspense>
     </div>
   );
 }
